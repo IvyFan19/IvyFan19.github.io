081e200c37896b457128537d9fe1dcc8a71c68bf
IvyFan19 <xinwei.fan19@gmail.com>
2020-08-19 20:45:23 -0700

    changed to Pro version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,27 @@
-!(function ($) {
+/**
+* Template Name: MyResume - v2.1.0
+* Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+!(function($) {
   "use strict";
 
   // Preloader
-  $(window).on('load', function () {
+  $(window).on('load', function() {
     if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
+      $('#preloader').delay(100).fadeOut('slow', function() {
         $(this).remove();
       });
     }
   });
 
   // Hero typed
-  if( $('.typed').length) {
+  if ($('.typed').length) {
     var typed_strings = $(".typed").data('typed-items');
     typed_strings = typed_strings.split(',')
     new Typed('.typed', {
-      strings:typed_strings,
+      strings: typed_strings,
       loop: true,
       typeSpeed: 100,
       backSpeed: 50,
@@ -29,7 +35,7 @@
       var target = $(this.hash);
       if (target.length) {
         e.preventDefault();
-        
+
         var scrollto = target.offset().top;
 
         $('html, body').animate({
@@ -51,7 +57,7 @@
   });
 
   // Activate smooth scroll on page load with hash links in the url
-  $(document).ready(function(){
+  $(document).ready(function() {
     if (window.location.hash) {
       var initial_nav = window.location.hash;
       if ($(initial_nav).length) {
@@ -82,27 +88,27 @@
   var nav_sections = $('section');
   var main_nav = $('.nav-menu, #mobile-nav');
 
-  $(window).on('scroll', function () {
+  $(window).on('scroll', function() {
     var cur_pos = $(this).scrollTop() + 300;
-  
+
     nav_sections.each(function() {
       var top = $(this).offset().top,
-          bottom = top + $(this).outerHeight();
-  
+        bottom = top + $(this).outerHeight();
+
       if (cur_pos >= top && cur_pos <= bottom) {
-        if( cur_pos <= bottom ) {
+        if (cur_pos <= bottom) {
           main_nav.find('li').removeClass('active');
         }
-        main_nav.find('a[href="#'+$(this).attr('id')+'"]').parent('li').addClass('active');
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
       }
-      if( cur_pos < 200 ) {
+      if (cur_pos < 200) {
         $(".nav-menu ul:first li:first").addClass('active');
       }
     });
   });
-  
+
   // Back to top button
-  $(window).scroll(function () {
+  $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
     } else {
@@ -110,7 +116,7 @@
     }
   });
 
-  $('.back-to-top').click(function () {
+  $('.back-to-top').click(function() {
     $('html, body').animate({
       scrollTop: 0
     }, 1500, 'easeInOutExpo');
@@ -128,7 +134,9 @@
     $('.progress .progress-bar').each(function() {
       $(this).css("width", $(this).attr("aria-valuenow") + '%');
     });
-  }, { offset: '80%'} );
+  }, {
+    offset: '80%'
+  });
 
   // Init AOS
   function aos_init() {
@@ -139,21 +147,25 @@
   }
 
   // Porfolio isotope and filter
-  $(window).on('load', function () {
+  $(window).on('load', function() {
     var portfolioIsotope = $('.portfolio-container').isotope({
       itemSelector: '.portfolio-item'
     });
 
-    $('#portfolio-flters li').on( 'click', function() {
+    $('#portfolio-flters li').on('click', function() {
       $("#portfolio-flters li").removeClass('filter-active');
       $(this).addClass('filter-active');
 
-      portfolioIsotope.isotope({ filter: $(this).data('filter') });
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
       aos_init();
     });
 
     // Initiate venobox (lightbox feature used in portofilo)
-    $('.venobox').venobox({'share': false}); 
+    $('.venobox').venobox({
+      'share': false
+    });
 
     // Initiate aos_init() function
     aos_init();
@@ -175,5 +187,5 @@
     loop: true,
     items: 1
   });
-  
+
 })(jQuery);
